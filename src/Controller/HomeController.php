e209ec81486f2eaf263fdb5afa253f9c5bda7d3c
Adib-Legastelois <adib.legastelois@icloud.com>
2020-10-01 17:31:43 +0100

    Create Property Entity + Home AND Property Controller && Twig Template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     * return Response
+     */
+
+     //Affiche les 4 dernieres maison 
+    public function index(PropertyRepository $propertyRepository) :Response
+    {
+        $properties = $propertyRepository->findLatest();
+        //dd($properties);
+        return $this->render('home/index.html.twig', compact('properties'));
+    }
+}
---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * ETAPE 0 Créer un objet dans la DB via sont Entity 
+     * $property = new Property();
+        *$property->setTitle('First House')
+        *    ->setPrice(300000)
+        *    ->setRooms(5)
+        *    ->setBedrooms(3)
+        *    ->setDescription('B T H')
+        *    ->setSurface(60)
+        *    ->setFloor(2)
+        *    ->setHeat(1)
+        *    ->setCity('NEW YORK')
+        *    ->setAdress('Greenfiel Park')
+        *    ->setPostalCode(10000);
+
+        *    $em = $this->getDoctrine()->getManager();
+        *    $em->persist($property);
+        *    $em->flush();
+    */
+
+    /**ETAPE 1 Injection de Dépendance des données via PropertyRepository dans le constructeur*/
+
+    private $propertyRepository;
+    private $em;
+
+    public function __construct(PropertyRepository $propertyRepository, EntityManagerInterface $em)
+    {
+       $this->propertyRepository = $propertyRepository;
+       $this->em = $em;
+    }
+        
+
+    /**ETAPE 2 RECUPERER LES DONNÉES DANS LA DB VIA L'OBJET REPOSITORY INJECTER DANS LE CONSTRUCT OU METHODE SELON PERTINANCE */
+
+    /**
+     * @Route("/properties", name="property.index")
+     */
+    public function index() :Response
+    {
+        /*Dans ce cas ci je récupère les maison non vendu en me servant de findAllUnsold() dans PropertyRepository
+        $property = $this->propertyRepository->findAllUnsold();
+        dd($property);*/
+
+        /*Détécte le changement d'état si je vend la maison par exemple
+        $this->em->flush();*/
+
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties',
+        ]);
+    }
+
+    /**ETAPE 3 Montrer le bien Selectioné avec slug et id */
+    /**
+     * @Route("/properties/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     */
+    public function show(Property $property, string $slug): Response //$property = $this->propertyRepository->find($id); Ce fait dans la méhode show grace a l'Injection
+    {
+        
+        if($property->getSlug() !== $slug){
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+        return $this->render('property/show.html.twig', [
+            'property' => $property,
+            'current_menu' => 'properties',
+        ]);
+    }
+}
